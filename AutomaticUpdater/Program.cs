529960f58f0f188ed108ef328918d548e96c6bc7
Boros Ákos <borosakos97@gmail.com>
2019-10-28 20:46:46 +0100

    Add project files.

---
+++ AutomaticUpdater/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticUpdater {
+    class Program {
+        static void Main(string[] args) {
+            Settings settings = new Settings();
+            settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText("sources\\settings.json"));
+            Console.WriteLine(settings.version);
+            Console.WriteLine(settings.user);
+            Console.ReadLine();
+        }
+    }
+}
